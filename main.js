28ad9d5ebd830fa2e421096c00148ead9a1daa9f
tgaff <tgaff@alumni.nd.edu>
2015-12-11 15:13:09 -0800

    handlebars templates

---
+++ main.js
@@ -0,0 +1,10 @@
+var source = $('#tracks-template').html();
+var template = Handlebars.compile(source);
+var searchUrl = "https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj";
+
+
+
+$(document).on('ready', function() {
+
+
+});
